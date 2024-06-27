c97612500dab3ab3c578a549d5bc1659977adb5f
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2024-06-27 13:05:39 +0530

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -8,4 +8,5 @@ my_news_api_key = os.getenv('NEWS_API_KEY')
 
 if my_news_api_key is None:
     raise ValueError("NEWS_API_KEY not found in environment variables. Please set it in GitHub secrets.")
+    
 
