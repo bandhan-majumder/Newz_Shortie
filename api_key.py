965e15ef99dbbd5f2f7d426c3d3e00d5a01c2ac4
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2024-06-27 12:55:13 +0530

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -1,1 +1,8 @@
-my_news_api_key = "ENTER YOUR NEWS API KEY" # get it from here -> https://newsapi.org/
+import os
+
+# Fetch the API key from GitHub secret
+news_api_key = os.getenv('NEWS_API_KEY')
+
+if news_api_key is None:
+    raise ValueError("NEWS_API_KEY not found in environment variables. Please set it in GitHub secrets.")
+
